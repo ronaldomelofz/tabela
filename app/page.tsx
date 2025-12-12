9669dc92fb7c53fac45719042df02bffc9cf6206
Ronaldo Melo <ronaldo@example.com>
2025-12-12 10:29:26 -0300

    Garante formato de moeda brasileiro com vírgula para centavos

--- app/page.tsx
+++ app/page.tsx
@@ -50,6 +50,8 @@ export default function Home() {
     return new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
     }).format(value);
   };
 
